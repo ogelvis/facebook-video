272772cc94b25abc4f4656929a4eaf7691946dd9
ogelvis <ogbidielvis@gmail.com>
2026-03-27 10:16:44 +0100

    fix fbDownloader import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const fbDownloader = require('@mrnima/facebook-downloader');
+const fbDownloader = require('@mrnima/facebook-downloader').default || require('@mrnima/facebook-downloader');
 
 const app = express();
 
